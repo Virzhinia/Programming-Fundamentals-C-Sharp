4a88b4c0baceb2021d8da629f48a7b03e3bbd2ca
Virzhinia <virzhinia.nikolova@gmail.com>
2018-02-21 21:18:48 +0200

    new ordering
    new ordering

---
+++ 06.ARRAYS/02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (var x in nums)
+            {
+                double rounded = Math.Round(x, 0, MidpointRounding.AwayFromZero);
+                Console.WriteLine(x + $"=> {rounded}");
+            }
+        }
+    }
+}
---
+++ 06.ARRAYS/10. Pairs by Difference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Pairs_by_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int k = i; k < numbers.Length; k++)
+                {
+                    if(Math.Abs(numbers[k]-numbers[i])==difference && i<k)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
